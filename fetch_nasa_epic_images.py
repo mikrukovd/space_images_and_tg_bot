e7f34fa07ce58fbb784351ece6c976cddc56eb1a
mikrukovd <z123456689z@gmail.com>
2025-05-04 21:16:23 +0300

    Update fetch_nasa_epic_images.py

--- fetch_nasa_epic_images.py
+++ fetch_nasa_epic_images.py
@@ -3,56 +3,60 @@ import os
 import argparse
 
 from dotenv import load_dotenv
-from helpers import get_file_name_and_file_extension, download_images
+from helpers import get_file_name_and_file_extension, download_image
 
 
-def download_nasa_epic_images(save_folder, date):
+def download_nasa_epic_images(save_folder, date, api):
     '''Скачивает изображения планеты по дате'''
 
-    load_dotenv()
-    api_nasa = os.getenv('API_NASA')
-
     base_url = 'https://api.nasa.gov/EPIC/api/natural'
+    params = {'api_key': api}
 
     if date:
-        url = f'{base_url}/date/{date}?api_key={api_nasa}'
+        url = f'{base_url}/date/{date}'
 
     else:
-        url = f'{base_url}/available?api_key={api_nasa}'
-        response = requests.get(url)
+        url = f'{base_url}/available'
+        response = requests.get(url, params)
         response.raise_for_status()
         available_dates = response.json()
+
         if available_dates:
             date = available_dates[-1]
-            url = f'{base_url}/date/{date}?api_key={api_nasa}'
+            url = f'{base_url}/date/{date}'
+
         else:
             print('Нет доступных дат')
             return
 
-    response = requests.get(url)
+    response = requests.get(url, params)
     response.raise_for_status()
-    data = response.json()
     image_urls = []
 
-    for item in data:
-        image_name = item['image']
+    for content in response.json():
+        image_name = content['image']
         year, month, day = date.split('-')
         image_url = (
             f'https://api.nasa.gov/EPIC/archive/natural/{year}/{month}/{day}'
-            f'/png/{image_name}.png?api_key={api_nasa}'
+            f'/png/{image_name}.png'
         )
-        image_urls.append(image_url)
+        response_image_url = requests.get(image_url, params)
+        response_image_url.raise_for_status()
+        image_urls.append(response_image_url.url)
 
-    for number, image in enumerate(image_urls):
-        splited_filename = get_file_name_and_file_extension(image_urls[number])
-        download_images(
-            image_urls[number],
+    for link in image_urls:
+        splited_filename = get_file_name_and_file_extension(link)
+        download_image(
+            link,
             save_folder,
             f'{splited_filename[0]}{splited_filename[1]}'
         )
 
 
 def main():
+    load_dotenv()
+    api_key_nasa = os.getenv('API_KEY_NASA')
+
     parser = argparse.ArgumentParser(description='epic')
     parser.add_argument(
         '--date',
@@ -67,7 +71,7 @@ def main():
 
     args = parser.parse_args()
 
-    download_nasa_epic_images(args.save_folder, args.date)
+    download_nasa_epic_images(args.save_folder, args.date, api_key_nasa)
 
 
 if __name__ == '__main__':
