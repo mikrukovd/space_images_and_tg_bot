4be859098df6af60dc6d8a65d774334db570e1a4
mikrukovd <z123456689z@gmail.com>
2025-05-20 12:13:10 +0300

    Update fetch_spacex_images.py

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,26 +1,26 @@
 import requests
 import argparse
 
-from helpers import download_image, get_file_name_and_file_extension
+from helpers import download_image, get_filename_and_file_extension
 
 
-def download_spacex_images(save_folder, launch_id=None):
+def download_spacex_images(save_folder, launch_id='latest'):
     '''Скачивает изображения SpaceX по id запуска'''
 
-    url = f'https://api.spacexdata.com/v5/launches/{launch_id or 'latest'}'
+    url = f'https://api.spacexdata.com/v5/launches/{launch_id}'
     response = requests.get(url)
     response.raise_for_status()
     image_spacex_urls = response.json()['links']['flickr']['original']
 
     for number, image in enumerate(image_spacex_urls):
-        splited_filename = get_file_name_and_file_extension(
+        filename, file_extension = get_filename_and_file_extension(
             image_spacex_urls[number]
         )
 
         download_image(
             image_spacex_urls[number],
             save_folder,
-            f'{splited_filename[0]}{splited_filename[1]}'
+            f'{filename}{file_extension}'
         )
 
 
@@ -28,11 +28,10 @@ def main():
     parser = argparse.ArgumentParser(
         description='''Скачивает изображения SpaceX по id запуска'''
     )
-
     parser.add_argument(
         'launch_id',
         nargs='?',
-        default=None,
+        default='latest',
         help='id запуска SpaceX'
     )
 
